05bc57f74e8ccbd936934491da6ed4573d81680f
Louis Pearson <opensource@louispearson.work>
2022-08-01 11:44:28 -0600

    Update build, add nix flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    zig.url = "github:arqv/zig-overlay";
+  };
+
+  outputs = { self, nixpkgs, zig, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+
+        # nix develop
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            zig.packages.${system}.master.latest
+            pkgs.butler
+          ];
+        };
+
+      });
+}
